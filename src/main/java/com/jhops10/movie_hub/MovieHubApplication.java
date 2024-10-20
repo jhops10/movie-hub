74ec3cf2ac57564ae44c444da92c0de22f5b6318
João Paulo <joaowais@gmail.com>
2024-10-20 19:57:12 -0300

    first commit

---
+++ src/main/java/com/jhops10/movie_hub/MovieHubApplication.java
@@ -0,0 +1,13 @@
+package com.jhops10.movie_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhops10/movie_hub/MovieHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhops10.movie_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
