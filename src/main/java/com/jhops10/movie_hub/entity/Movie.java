d21acfc84bf5e439bcacdb1fc092b2543fe44c91
João Paulo <joaowais@gmail.com>
2024-10-21 11:35:25 -0300

    Altera strategy do GeneratedValue para IDENTITY

--- src/main/java/com/jhops10/movie_hub/entity/Movie.java
+++ src/main/java/com/jhops10/movie_hub/entity/Movie.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class Movie {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
